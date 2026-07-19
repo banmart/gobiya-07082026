38448b893656e3d88ae00aa9a2e863db00c328c3
Steve <banmart@gmail.com>
2026-07-19 06:19:22 -0700

    feat: add Quickpass logo to client marquee

--- app/page.js
+++ app/page.js
@@ -48,6 +48,7 @@ const CLIENT_LOGOS = [
   { src: '/assets/img/safetycentric-logo.png', alt: 'SafetyCentric' },
   { src: '/assets/img/dgplumbing-logo.webp', alt: 'DG Plumbing' },
   { src: '/assets/img/mtw-logo-mark-nFG9LpnH.webp', alt: 'MTW' },
+  { src: '/assets/img/quickpass-logo.webp', alt: 'Quickpass' },
   { src: '/assets/img/client-5.webp', alt: 'Gobiya client' },
 ];
 
